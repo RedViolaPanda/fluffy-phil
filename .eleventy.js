aa7b538c0ffe6e2c03503e71e22562c33e75cc39
huijing <huijing@hey.com>
2020-09-13 23:41:59 +0800

    Add more pages and content

--- .eleventy.js
+++ .eleventy.js
@@ -6,9 +6,19 @@ module.exports = function(eleventyConfig) {
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' }
     return date.toLocaleDateString('en-SG', options)
   })
+  eleventyConfig.addNunjucksFilter('limit', (arr, limit) => arr.slice(0, limit))
+
   eleventyConfig.addShortcode('img2x', (filename, ext, alt, classname) => {
     return `<img src="/assets/img/${filename}.${ext}" srcset="/assets/img/${filename}@2x.${ext} 2x" alt="${alt ? alt : ''}" class="${classname ? classname : ''}">`
   })
+
+  eleventyConfig.addPairedShortcode('postImg', (filename, ext) => {
+    return `<img src="/assets/img/stories/${filename}.${ext}" srcset="/assets/img/stories/${filename}@2x.${ext} 2x" alt="" class="post-image">`;
+  });
+
+  eleventyConfig.addPairedShortcode('musicianImg', (filename) => {
+    return `<img src="/assets/img/musicians/${filename}.png" srcset="/assets/img/musicians/${filename}@2x.png 2x" alt="" class="musician-image">`;
+  });
 
   return {
     templateFormats: [
--- src/views/posts/2020-09-03-why-doesnt-the-moon-fly-away.md
+++ src/views/posts/2020-09-03-why-doesnt-the-moon-fly-away.md
@@ -4,6 +4,8 @@ tags: post
 date: 2020-09-15
 layout: posts.njk
 permalink: /{{ title | slug }}/index.html
+image: 01-image
 ---
+{% img2x '/stories/01-comic', 'png', 'Why doesn&apos;t the moon fly away?', 'comic' %}
 
-https://imslp.org/wiki/Viola_Concerto%2C_TWV_51:G9_(Telemann%2C_Georg_Philipp)
+View the [musical score for the Viola Concerto](https://imslp.org/wiki/Viola_Concerto%2C_TWV_51:G9_(Telemann%2C_Georg_Philipp)).
