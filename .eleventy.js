6ef27f01748f6d9169541c562583cc60412cb944
huijing <huijing@hey.com>
2020-10-01 15:53:16 +0800

    Modify shortcodes

--- .eleventy.js
+++ .eleventy.js
@@ -12,12 +12,20 @@ module.exports = function(eleventyConfig) {
     return `<img src="/assets/img/${filename}.${ext}" srcset="/assets/img/${filename}@2x.${ext} 2x" alt="${alt ? alt : ''}" class="${classname ? classname : ''}">`
   })
 
-  eleventyConfig.addPairedShortcode('postImg', (filename, ext) => {
-    return `<img src="/assets/img/stories/${filename}.${ext}" srcset="/assets/img/stories/${filename}@2x.${ext} 2x" alt="" class="post-image">`;
+  eleventyConfig.addShortcode('img4w', (filename, ext, alt, classname) => {
+    return `<img src="/assets/img/${filename}-640.${ext}" srcset="/assets/img/${filename}-480.${ext} 480w, /assets/img/${filename}-640.${ext} 640w, /assets/img/${filename}-960.${ext} 960w, /assets/img/${filename}-1280.${ext} 1280w" sizes="(max-width: 400px) 100vw, (max-width: 960px) 75vw, 640px" alt="${alt ? alt : ''}" class="${classname ? classname : ''}">`
+  })
+
+  eleventyConfig.addPairedShortcode('postImg', (filename, ext, alt) => {
+    return `<img src="/assets/img/stories/${filename}.${ext}" srcset="/assets/img/stories/${filename}@2x.${ext} 2x" alt="${alt ? alt : ''}" class="post-image">`;
   });
 
-  eleventyConfig.addPairedShortcode('musicianImg', (filename) => {
-    return `<img src="/assets/img/musicians/${filename}.png" srcset="/assets/img/musicians/${filename}@2x.png 2x" alt="" class="musician-image">`;
+  eleventyConfig.addPairedShortcode('musicianImg', (filename, alt) => {
+    return `<img src="/assets/img/musicians/${filename}.png" srcset="/assets/img/musicians/${filename}@2x.png 2x" alt="${alt ? alt : ''}" class="musician-image">`;
+  });
+
+  eleventyConfig.addPairedShortcode('stickerImg', (filename, alt) => {
+    return `<img src="/assets/img/stickers/${filename}.png" srcset="/assets/img/stickers/${filename}@2x.png 2x" alt="${alt ? alt : ''}" class="sticker-image">`;
   });
 
   return {
